05c723628cc5623abe593b3f7554173181561ea6
Pieter Van Neste <pieter@dreamsandcreations.be>
2016-10-25 16:49:20 +0200

    package.json replace branch + publish feature

--- git-flow-feature-start-all.sh
+++ git-flow-feature-start-all.sh
@@ -6,6 +6,11 @@ for file in */ .*/ ; do
     	echo "$(tput setaf 3)--- $file ---$(tput sgr0)"
        cd $file
        git flow feature start $1
+       sed '/git@github.com:Moovly/s/.git#.*/\.git#feature\\\/'$1'\",/' <package.json >tmp.json
+       mv tmp.json package.json
        cd ../
     fi
 done
+git-add-all
+git-commit-all "init feature"
+git-flow-feature-publish-all $1
