f964feea4b24391b135c689f6ab2259571e3ff9b
Pieter Van Neste <pieter@dreamsandcreations.be>
2016-12-05 13:19:46 +0100

    git-checkout-all added

--- init.sh
+++ init.sh
@@ -4,10 +4,8 @@ for f in *.sh
 do
 
 	 if [[ "$f" != "init.sh" ]] ; then
-		#echo "Create $f"
-		mkdir -p ~/git-all-scripts/ 
-		cp $f ~/git-all-scripts/
-		chmod +x ~/git-all-scripts/$f
+		echo "Create $f"
+		chmod +x $f
 		echo "alias ${f%%.*}=\"~/git-all-scripts/$f"\"
 		alias ${f%%.*}=~/git-all-scripts/$f
 	fi
